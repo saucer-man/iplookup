61c7e9777266764548029d6acea4b1c6c1c0dd6f
saucer-man <gyq0929@gmail.com>
2021-07-28 19:03:38 +0800

    fix package name

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package lookup
 
 import (
 	"context"
@@ -7,23 +7,22 @@ import (
 	"github.com/saucer-man/iplookup/runner"
 
 	_ "github.com/projectdiscovery/fdmax/autofdmax"
-	"github.com/projectdiscovery/gologger"
 )
 
-func main() {
-	// Parse the command line flags and read config files
-	options := runner.ParseOptions()
+// func main() {
+// 	// Parse the command line flags and read config files
+// 	options := runner.ParseOptions()
 
-	newRunner, err := runner.NewRunner(options)
-	if err != nil {
-		gologger.Fatal().Msgf("Could not create runner: %s\n", err)
-	}
+// 	newRunner, err := runner.NewRunner(options)
+// 	if err != nil {
+// 		gologger.Fatal().Msgf("Could not create runner: %s\n", err)
+// 	}
 
-	err = newRunner.RunEnumeration(context.Background())
-	if err != nil {
-		gologger.Fatal().Msgf("Could not run enumeration: %s\n", err)
-	}
-}
+// 	err = newRunner.RunEnumeration(context.Background())
+// 	if err != nil {
+// 		gologger.Fatal().Msgf("Could not run enumeration: %s\n", err)
+// 	}
+// }
 
 func LookUp(ip string) {
 	options := runner.Options{
